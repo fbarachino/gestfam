eb22f61fd110784b426a6c316d3013939620c1bc
fbarachino <flavio.barachino@gmail.com>
2020-12-07 16:01:15 +0000

    test widget su dashboard

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -63,16 +63,7 @@
             <div class="sidebar-nav navbar-collapse">
 
                 <ul class="nav" id="side-menu">
-                    <li class="sidebar-search">
-                        <div class="input-group custom-search-form">
-                            <input type="text" class="form-control" placeholder="Search...">
-                                <span class="input-group-btn">
-                                    <button class="btn btn-primary" type="button">
-                                        <i class="fa fa-search"></i>
-                                    </button>
-                                </span>
-                        </div>
-                    </li>
+            @include('menus.searchbar')        
                     <!-- INIZIO MENU -->
 		    @include('menus.mainMenu')
                     <!-- FINE MENU -->
